a3161b2a87a79f67c660bce04d36cb373d0d704d
mraddy1 <radhemohan15aug1947@gmail.com>
2026-02-22 23:21:45 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    
+    printf("enter first number");
+    scanf("%f",&a);
+    
+    printf("enter second number");
+    scanf("%f",&b);
+    
+    printf("sum = %.2f\n", a + b);
+    printf("difference = %.2\n", a - b);
+    printf("product = %.2f\n", a*b);
+    printf("divison = %f.2\n", a/b);
+    
+    return 0;
+    
+}
